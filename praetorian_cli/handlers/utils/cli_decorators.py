a9c1042bb42bf1ce4879f3f8b2af7b6f56b207bc
Peter Kwan <peter.kwan@praetorian.com>
2024-06-14 12:38:39 -0700

    Removed dead code path

--- praetorian_cli/handlers/utils/cli_decorators.py
+++ praetorian_cli/handlers/utils/cli_decorators.py
@@ -90,8 +90,6 @@ def process_with_script(script_name, output, cli_kwargs):
         if hasattr(script_module, 'process') and len(signature(script_module.__dict__['process']).parameters) == 4:
             ctx = click.get_current_context()
             controller = ctx.obj
-            if ctx.parent.command.name == 'cli':
-                cmd = dict(product=ctx.command.name, action=None, type=None)
             if ctx.parent.parent.command.name == 'cli':
                 cmd = dict(product=ctx.parent.command.name, action=ctx.command.name, type=None)
             else:
